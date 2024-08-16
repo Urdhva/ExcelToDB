d8295f633918b5368f863b74a2bf9b32470c53ef
Urdhva <urdhva.borse@gmail.com>
2024-08-16 19:31:27 +0530

    added new functionality

--- scharapurge.py
+++ scharapurge.py
@@ -3,37 +3,55 @@
 #1. Take in a csv or (convert an excel to csv if excel is given).
 #2. Go through each column until you reach they key 'name'.
 #3. Replace the name containing special characters and honorifics with the truncated name.
+# ^To do the above step, truncate every character that isn't in a certain unicode range
 #4. Voila
 
 #dataframe management
 import pandas as pd
 import magic 
+import csv
 
 def getFile():
-    file = ''
+    file_path = ''
     fType = ''
 
     try:
-        file = input("Enter file: ")
+        file_path = input("Enter file: ")
         #check if file exists (This method actually check file type, but the file needs to exist for it to check type)
-        fType = magic.from_file(file)
+        fType = magic.from_file(file_path)
     except Exception as e:
         print(e)
         return
+    print("Correct file type")
+    return file_path
 
-    return file
 
-def openFile(file):
-    pass
+def openFile(file_path):
+    with open(file_path, newline='', encoding="utf8") as file1:
+        reader = csv.DictReader(file1)
+
+        #iterates through each row
+        
+        for doc in reader:
+            # print(doc["Username"])
+            deliminator = ""
+            remadeString = "".join(ch for ch in doc["Username"] if ch.isalnum())
+            #we join ch to remadeString if ch(an iterable btw) is alpha numerical
+
+            print(remadeString)
+            
+                
+        
+
 
 def execute():
     #why check file type when you check if file exists. 
     # OR you could check file type during in the execute function
-    file = getFile()
-    if file == None:
+    file_path = getFile()
+    if file_path == None:
         return
     
-    openFile(file)
+    openFile(file_path)
 
 
 def main():
